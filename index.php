08660b18e87a400e0c9015d277108bb5cf2ef959
Dr Bean <drbean@freeshell.org>
2017-08-28 15:28:06 +0800

    no_CFG_object.use__DIR__

--- index.php
+++ index.php
@@ -22,8 +22,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @package   core_group
  */
-global $CFG;
-require_once($CFG->dirroot . '/config.php');
+require_once(__DIR__ . '../../../../config.php');
 require_once('lib.php');
 
 $courseid = required_param('id', PARAM_INT);
