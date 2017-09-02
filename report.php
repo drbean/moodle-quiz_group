245bae367ce29861e8a0fb4c50433936fa1a99b7
Dr Bean <drbean@freeshell.org>
2017-09-02 14:31:40 +0800

    extends.s/responses/attempts/,which_responses_extends

--- report.php
+++ report.php
@@ -42,7 +42,7 @@ require_once($CFG->dirroot . '/mod/quiz/report/responses/report.php');
  * @copyright 1999 onwards Martin Dougiamas and others {@link http://moodle.com}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class quiz_group_report extends quiz_responses_report {
+class quiz_group_report extends quiz_attempts_report {
 
     public function display($quiz, $cm, $course) {
 	    parent::display($quiz, $cm, $course);
