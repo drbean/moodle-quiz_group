ce5f200002f488ea9b964543aa2779ec6c627977
Dr Bean <drbean@freeshell.org>
2017-09-02 15:15:29 +0800

    copied_responses_display,overview_similar.reformated

--- report.php
+++ report.php
@@ -45,6 +45,110 @@ require_once($CFG->dirroot . '/mod/quiz/report/responses/report.php');
 class quiz_group_report extends quiz_attempts_report {
 
     public function display($quiz, $cm, $course) {
-	    parent::display($quiz, $cm, $course);
+        global $OUTPUT, $DB;
+
+        list($currentgroup, $studentsjoins, $groupstudentsjoins, $allowedjoins)
+            = $this->init('responses', 'quiz_responses_settings_form',
+                $quiz, $cm, $course);
+
+        $options = new quiz_responses_options('responses', $quiz, $cm, $course);
+
+        if ($fromform = $this->form->get_data()) {
+            $options->process_settings_from_form($fromform);
+
+        } else {
+            $options->process_settings_from_params();
+        }
+
+        $this->form->set_data($options->get_initial_form_data());
+
+        // Load the required questions.
+        $questions = quiz_report_get_significant_questions($quiz);
+
+        // Prepare for downloading, if applicable.
+        $courseshortname = format_string($course->shortname, true,
+                array('context' => context_course::instance($course->id)));
+        if ($options->whichtries === question_attempt::LAST_TRY) {
+            $tableclassname = 'quiz_last_responses_table';
+        } else {
+            $tableclassname = 'quiz_first_or_all_responses_table';
+        }
+        $table = new $tableclassname($quiz, $this->context, $this->qmsubselect,
+            $options, $groupstudentsjoins, $studentsjoins,
+            $questions, $options->get_url());
+        $filename = quiz_report_download_filename(get_string(
+            'responsesfilename', 'quiz_responses'),
+            $courseshortname, $quiz->name);
+        $table->is_downloading($options->download, $filename,
+                $courseshortname . ' ' . format_string($quiz->name, true));
+        if ($table->is_downloading()) {
+            raise_memory_limit(MEMORY_EXTRA);
+        }
+
+        $this->hasgroupstudents = false;
+        if (!empty($groupstudentsjoins->joins)) {
+            $sql = "SELECT DISTINCT u.id
+                      FROM {user} u
+                    $groupstudentsjoins->joins
+                     WHERE $groupstudentsjoins->wheres";
+            $this->hasgroupstudents = $DB->record_exists_sql($sql,
+                $groupstudentsjoins->params);
+        }
+        $hasstudents = false;
+        if (!empty($studentsjoins->joins)) {
+            $sql = "SELECT DISTINCT u.id
+                    FROM {user} u
+                    $studentsjoins->joins
+                    WHERE $studentsjoins->wheres";
+            $hasstudents = $DB->record_exists_sql($sql, $studentsjoins->params);
+        }
+        if ($options->attempts == self::ALL_WITH) {
+            // This option is only available to users who can access all groups
+            // in groups mode, so setting allowed to empty (which means all
+            // quiz attempts are accessible, is not a security problem.
+            $allowedjoins = new \core\dml\sql_join();
+        }
+
+        $this->process_actions($quiz, $cm, $currentgroup, $groupstudentsjoins,
+            $allowedjoins, $options->get_url());
+
+        // Start output.
+        if (!$table->is_downloading()) {
+            // Only print headers if not asked to download data.
+            $this->print_header_and_tabs($cm, $course, $quiz, $this->mode);
+        }
+
+        if ($groupmode = groups_get_activity_groupmode($cm)) {
+            // Groups are being used, so output the group selector if we are
+            // not downloading.
+            if (!$table->is_downloading()) {
+                groups_print_activity_menu($cm, $options->get_url());
+            }
+        }
+
+        // Print information on the number of existing attempts.
+        if (!$table->is_downloading()) {
+            // Do not print notices when downloading.
+            if ($strattemptnum = quiz_num_attempt_summary($quiz, $cm, true,
+                $currentgroup)) {
+                echo '<div class="quizattemptcounts">' . $strattemptnum .
+                    '</div>';
+            }
+        }
+
+        $hasquestions = quiz_has_questions($quiz->id);
+        if (!$table->is_downloading()) {
+            if (!$hasquestions) {
+                echo quiz_no_questions_message($quiz, $cm, $this->context);
+            } else if (!$hasstudents) {
+                echo $OUTPUT->notification(get_string('nostudentsyet'));
+            } else if ($currentgroup && !$this->hasgroupstudents) {
+                echo $OUTPUT->notification(get_string('nostudentsingroup'));
+            }
+
+            // Print the display options.
+            $this->form->display();
+        }
+
     }
 }
