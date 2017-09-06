a15c1bdef0a341000dc9517e67cce1c2f34a9287
Dr Bean <drbean@freeshell.org>
2017-09-06 20:28:06 +0800

    back out f56e21d15b0745add7d234f7d00a763fef499276 quiz_last_group_table,quiz_first_or_all_group_table

--- report.php
+++ report.php
@@ -72,9 +72,9 @@ class quiz_group_report extends quiz_attempts_report {
         $courseshortname = format_string($course->shortname, true,
                 array('context' => context_course::instance($course->id)));
         if ($options->whichtries === question_attempt::LAST_TRY) {
-            $tableclassname = 'quiz_group_table';
+            $tableclassname = 'quiz_last_group_table';
         } else {
-            $tableclassname = 'quiz_group_table';
+            $tableclassname = 'quiz_first_or_all_group_table';
         }
         $table = new $tableclassname($quiz, $this->context, $this->qmsubselect,
             $options, $groupstudentsjoins, $studentsjoins,
