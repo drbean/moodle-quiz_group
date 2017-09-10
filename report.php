3a685d1d2296e3440e0f4847739241cb1c94aae5
Dr Bean <drbean@freeshell.org>
2017-09-10 17:02:31 +0800

    all_group_menu=group_menu?

--- report.php
+++ report.php
@@ -187,8 +187,9 @@ class quiz_group_report extends quiz_attempts_report {
             // not downloading.
             if (!$table->is_downloading()) {
                 $group_menu = groups_print_activity_menu($cm, $options->get_url(), true, false);
-                $all_group_menu = preg_replace( 'participants',
-                    'groups', $group_menu, 1 );
+                //$all_group_menu = preg_replace( 'participants',
+                //    'groups', $group_menu, 1 );
+                $all_group_menu = $group_menu;
                 echo $all_group_menu;
             }
         }
