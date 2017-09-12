fd8c72d336b3ce319bd8598c96f26faf8476d65f
Dr Bean <drbean@freeshell.org>
2017-09-12 09:47:24 +0800

    no_groupid

--- report.php
+++ report.php
@@ -96,7 +96,7 @@ class quiz_group_report extends quiz_attempts_report {
 
         $allgroups = groups_get_all_groups($cm->id, 0, 0, 'g.*', false);
 
-        foreach ( $allgroups as $groupid => $currentgroup )
+        foreach ( $allgroups as $currentgroup )
         {
             $empty = new \core\dml\sql_join();
             if ($currentgroup == self::NO_GROUPS_ALLOWED) {
