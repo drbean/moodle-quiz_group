92cd1930e5692da20c8753a83c006b923dbb0d2b
Dr Bean <drbean@freeshell.org>
2017-09-14 13:22:26 +0800

    allgroups.ids:notification

--- report.php
+++ report.php
@@ -100,9 +100,10 @@ class quiz_group_report extends quiz_attempts_report {
         $groupid = $currentgroup_object->id;
         echo $OUTPUT->notification( "groupid" . $groupid );
 
-        //foreach ( $allgroups as $currentgroup_object )
-        //{
-        //    $currentgroup = $currentgroup_object->id;
+        foreach ( $allgroups as $currentgroup_object )
+        {
+            $currentgroup = $currentgroup_object->id;
+            echo $OUTPUT->notification( "groupid" . $currentgroup );
         //    $empty = new \core\dml\sql_join();
 
         //    if ($currentgroup == self::NO_GROUPS_ALLOWED) {
@@ -123,7 +124,7 @@ class quiz_group_report extends quiz_attempts_report {
 
         //        return array($currentgroup, $studentsjoins, $groupstudentsjoins, $groupstudentsjoins);
         //    }
-        //}
+        }
         $this->currentgroup = $this->get_current_group($cm, $course, $this->context);
         $empty = new \core\dml\sql_join();
         if ($currentgroup == self::NO_GROUPS_ALLOWED) {
