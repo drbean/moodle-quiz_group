8dfe7311ded4f567cd4200a676c2dd72ece59254
Dr Bean <drbean@freeshell.org>
2017-09-14 19:33:34 +0800

    get_string.out:notification

--- report.php
+++ report.php
@@ -145,10 +145,10 @@ class quiz_group_report extends quiz_attempts_report {
         $this->print_header_and_tabs($cm, $course, $quiz, $this->mode);
 
         if ($groupmode = groups_get_activity_groupmode($cm)) {
-                echo $OUTPUT->notification(get_string("groupmode: visible?"));
+                echo $OUTPUT->notification("groupmode: visible?");
             // Groups are being used, so output the group selector
             $group_menu = groups_print_activity_menu($cm, $options->get_url(), true, false);
-            echo $OUTPUT->notification(get_string("groupmenu: $group_menu?"));
+            echo $OUTPUT->notification("groupmenu: $group_menu?");
             echo $group_menu;
         }
 
