cdc0e9e57f14132230ab56a8048938625fd5bcd5
Dr Bean <drbean@freeshell.org>
2017-09-14 15:33:44 +0800

    Revert "allgroupdata_after_options_in_display.messy!"
    This reverts commit 34de4f96e66f3e1e2bf93996ca32eaea32c32e77.
    
    Moodle: Invalid state passed to moodle_page::set_state. We are in state
    2 and state 1 was requested.

--- report.php
+++ report.php
@@ -70,11 +70,11 @@ class quiz_group_report extends quiz_attempts_report {
     protected function init($mode, $formclass, $quiz, $cm, $course) {
         $this->mode = $mode;
         $this->context = context_module::instance($cm->id);
-        $allgroupdata = $this->get_students_joins( $cm, $course);
+        list($currentgroup, $studentsjoins, $groupstudentsjoins, $allowedjoins) = $this->get_students_joins( $cm, $course);
         $this->qmsubselect = quiz_report_qm_filter_select($quiz);
         $this->form = new $formclass($this->get_base_url(),
-                array('quiz' => $quiz, 'currentgroup' => 122, 'context' => $this->context));
-        return $allgroupdata;
+                array('quiz' => $quiz, 'currentgroup' => $currentgroup, 'context' => $this->context));
+        return array($currentgroup, $studentsjoins, $groupstudentsjoins, $allowedjoins);
     }
 
     /**
@@ -104,122 +104,140 @@ class quiz_group_report extends quiz_attempts_report {
         foreach ( range(113, 139) as $currentgroup )
         {
             echo $OUTPUT->notification( "allgroup groupid" . $currentgroup );
-            $empty = new \core\dml\sql_join();
+        //    $empty = new \core\dml\sql_join();
 
-            if ($currentgroup == self::NO_GROUPS_ALLOWED) {
-                $alldata[$currentgroup] = array($currentgroup, $empty, $empty, $empty);
-            }
+        //    if ($currentgroup == self::NO_GROUPS_ALLOWED) {
+        //        return array($currentgroup, $empty, $empty, $empty);
+        //    }
 
-            $studentsjoins = get_enrolled_with_capabilities_join($this->context);
+        //    $studentsjoins = get_enrolled_with_capabilities_join($this->context);
 
-            if (empty($currentgroup)) {
-                $alldata[$currentgroup] = array($currentgroup, $studentsjoins, $empty, $studentsjoins);
-            }
+        //    if (empty($currentgroup)) {
+        //        return array($currentgroup, $studentsjoins, $empty, $studentsjoins);
+        //    }
 
-            // We have a currently selected group.
-            $groupstudentsjoins = get_enrolled_with_capabilities_join($this->context, '',
-                array('mod/quiz:attempt', 'mod/quiz:reviewmyattempts'), $currentgroup);
+        //    if (!empty (groups_group_exists($currentgroup ))) 
+        //    {
+        //        // We have a currently selected group.
+        //        $groupstudentsjoins = get_enrolled_with_capabilities_join($this->context, '',
+        //            array('mod/quiz:attempt', 'mod/quiz:reviewmyattempts'), $currentgroup);
 
-            $alldata[$currentgroup] = array($currentgroup, $studentsjoins, $groupstudentsjoins, $groupstudentsjoins);
+        //        return array($currentgroup, $studentsjoins, $groupstudentsjoins, $groupstudentsjoins);
+        //    }
         }
-        return $alldata;
+        $currentgroup = 122;
+        $empty = new \core\dml\sql_join();
+        if ($currentgroup == self::NO_GROUPS_ALLOWED) {
+            return array($currentgroup, $empty, $empty, $empty);
+        }
+
+        $studentsjoins = get_enrolled_with_capabilities_join($this->context);
+
+        if (empty($currentgroup)) {
+            echo $OUTPUT->notification( "no groupid" . $currentgroup );
+            return array($currentgroup, $studentsjoins, $empty, $studentsjoins);
+        }
+
+        $groupstudentsjoins = get_enrolled_with_capabilities_join($this->context, '',
+            array('mod/quiz:attempt', 'mod/quiz:reviewmyattempts'), $currentgroup);
+        return array($this->currentgroup, $studentsjoins, $groupstudentsjoins, $groupstudentsjoins);
     }
 
 
     public function display($quiz, $cm, $course) {
         global $OUTPUT, $DB;
 
-        $allgroupdata = $this->init('group', 'quiz_group_settings_form',
+        list($currentgroup, $studentsjoins, $groupstudentsjoins, $allowedjoins)
+            = $this->init('group', 'quiz_group_settings_form',
                 $quiz, $cm, $course);
 
         $options = new quiz_group_options('group', $quiz, $cm, $course);
+
         if ($fromform = $this->form->get_data()) {
             $options->process_settings_from_form($fromform);
+
         } else {
             $options->process_settings_from_params();
         }
+
         $this->form->set_data($options->get_initial_form_data());
+
         // Load the required questions.
         $questions = quiz_report_get_significant_questions($quiz);
 
-        foreach ( $allgroupdata as $groupid => $groupdata )
-        {
-            list($currentgroup, $studentsjoins, $groupstudentsjoins, $allowedjoins)
-                = $groupdata;
-
-            // Prepare for downloading, if applicable.
-            $courseshortname = format_string($course->shortname, true,
+        // Prepare for downloading, if applicable.
+        $courseshortname = format_string($course->shortname, true,
                 array('context' => context_course::instance($course->id)));
-            if ($options->whichtries === question_attempt::LAST_TRY) {
-                $tableclassname = 'quiz_last_responses_table';
-            } else {
-                $tableclassname = 'quiz_first_or_all_responses_table';
-            }
-            $table = new $tableclassname($quiz, $this->context, $this->qmsubselect,
-                $options, $groupstudentsjoins, $studentsjoins,
-                $questions, $options->get_url());
+        if ($options->whichtries === question_attempt::LAST_TRY) {
+            $tableclassname = 'quiz_last_responses_table';
+        } else {
+            $tableclassname = 'quiz_first_or_all_responses_table';
+        }
+        $table = new $tableclassname($quiz, $this->context, $this->qmsubselect,
+            $options, $groupstudentsjoins, $studentsjoins,
+            $questions, $options->get_url());
 
-            $this->hasgroupstudents = false;
-            if (!empty($groupstudentsjoins->joins)) {
-                $sql = "SELECT DISTINCT u.id
-                    FROM {user} u
+        $this->hasgroupstudents = false;
+        if (!empty($groupstudentsjoins->joins)) {
+            $sql = "SELECT DISTINCT u.id
+                      FROM {user} u
                     $groupstudentsjoins->joins
-                    WHERE $groupstudentsjoins->wheres";
-                $this->hasgroupstudents = $DB->record_exists_sql($sql,
-                    $groupstudentsjoins->params);
-            }
-            $hasstudents = false;
-            if (!empty($studentsjoins->joins)) {
-                $sql = "SELECT DISTINCT u.id
+                     WHERE $groupstudentsjoins->wheres";
+            $this->hasgroupstudents = $DB->record_exists_sql($sql,
+                $groupstudentsjoins->params);
+        }
+        $hasstudents = false;
+        if (!empty($studentsjoins->joins)) {
+            $sql = "SELECT DISTINCT u.id
                     FROM {user} u
                     $studentsjoins->joins
                     WHERE $studentsjoins->wheres";
-                $hasstudents = $DB->record_exists_sql($sql, $studentsjoins->params);
-            }
-            if ($options->attempts == self::ALL_WITH) {
-                // This option is only available to users who can access all groups
-                // in groups mode, so setting allowed to empty (which means all
-                // quiz attempts are accessible, is not a security problem.
-                $allowedjoins = new \core\dml\sql_join();
-            }
+            $hasstudents = $DB->record_exists_sql($sql, $studentsjoins->params);
+        }
+        if ($options->attempts == self::ALL_WITH) {
+            // This option is only available to users who can access all groups
+            // in groups mode, so setting allowed to empty (which means all
+            // quiz attempts are accessible, is not a security problem.
+            $allowedjoins = new \core\dml\sql_join();
+        }
 
-            $this->process_actions($quiz, $cm, $currentgroup, $groupstudentsjoins,
-                $allowedjoins, $options->get_url());
+        $this->process_actions($quiz, $cm, $currentgroup, $groupstudentsjoins,
+            $allowedjoins, $options->get_url());
 
-            // Start output.
+        // Start output.
+        if (!$table->is_downloading()) {
+            // Only print headers if not asked to download data.
+            $this->print_header_and_tabs($cm, $course, $quiz, $this->mode);
+        }
+
+        if ($groupmode = groups_get_activity_groupmode($cm)) {
+            // Groups are being used, so output the group selector if we are
+            // not downloading.
             if (!$table->is_downloading()) {
-                // Only print headers if not asked to download data.
-                $this->print_header_and_tabs($cm, $course, $quiz, $this->mode);
+                $group_menu = groups_print_activity_menu($cm, $options->get_url(), true, false);
+            }
+        }
+
+        // Print information on the number of existing attempts.
+        if (!$table->is_downloading()) {
+            // Do not print notices when downloading.
+            if ($strattemptnum = quiz_num_attempt_summary($quiz, $cm, true,
+                $currentgroup)) {
+                echo '<div class="quizattemptcounts">' . $strattemptnum .
+                    '</div>';
+            }
+        }
+
+        $hasquestions = quiz_has_questions($quiz->id);
+        if (!$table->is_downloading()) {
+            if (!$hasquestions) {
+                echo quiz_no_questions_message($quiz, $cm, $this->context);
+            } else if (!$hasstudents) {
+                echo $OUTPUT->notification(get_string('nostudentsyet'));
+            } else if ($currentgroup && !$this->hasgroupstudents) {
+                echo $OUTPUT->notification(get_string('nostudentsingroup'));
             }
 
-            if ($groupmode = groups_get_activity_groupmode($cm)) {
-                // Groups are being used, so output the group selector if we are
-                // not downloading.
-                if (!$table->is_downloading()) {
-                    $group_menu = groups_print_activity_menu($cm, $options->get_url(), true, false);
-                }
-            }
-
-            // Print information on the number of existing attempts.
-            if (!$table->is_downloading()) {
-                // Do not print notices when downloading.
-                if ($strattemptnum = quiz_num_attempt_summary($quiz, $cm, true,
-                    $currentgroup)) {
-                    echo '<div class="quizattemptcounts">' . $strattemptnum .
-                        '</div>';
-                }
-            }
-
-            $hasquestions = quiz_has_questions($quiz->id);
-            if (!$table->is_downloading()) {
-                if (!$hasquestions) {
-                    echo quiz_no_questions_message($quiz, $cm, $this->context);
-                } else if (!$hasstudents) {
-                    echo $OUTPUT->notification(get_string('nostudentsyet'));
-                } else if ($currentgroup && !$this->hasgroupstudents) {
-                    echo $OUTPUT->notification(get_string('nostudentsingroup'));
-                }
-            }
             // Print the display options.
             $this->form->display();
         }
